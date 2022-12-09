372a7746608e61d0e10642a165d801d83945ce25
ZhouSusan <szhou089@gmail.com>
2022-12-08 18:09:51 -0800

    Added PrintAllValues() that prints all the values from 1 to 255 in Fundamentals1/Program.cs

--- Fundamentals1/Program.cs
+++ Fundamentals1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Fundaamentals
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Print All Values from 1 to 255");
+            printAllValues(255);
+        }
+
+        //A program that prints all the values from 1-255
+        static void printAllValues(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
